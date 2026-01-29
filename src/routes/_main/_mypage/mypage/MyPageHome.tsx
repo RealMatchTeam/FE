df36090b02e318acea5c43fc690dab26c6773ccf
yeahsel <yeahsel0608@gmail.com>
2026-01-30 01:08:59 +0900

    fix: 린트 오류 해결

--- src/routes/_main/_mypage/mypage/MyPageHome.tsx
+++ src/routes/_main/_mypage/mypage/MyPageHome.tsx
@@ -1,4 +1,4 @@
-import { useEffect, useState } from "react";
+import { useState } from "react";
 import ConfirmModal from "../components/ConfirmModal";
 
 type Props = {
@@ -36,15 +36,9 @@ export default function MyPageHome({
   onLogout,
   onWithdraw,
 }: Props) {
-  const [openGate, setOpenGate] = useState(false);
+  const [openGate, setOpenGate] = useState(!hasMatchingTest);
   const [openLogout, setOpenLogout] = useState(false);
   const [openWithdraw, setOpenWithdraw] = useState(false);
-
-  useEffect(() => {
-    if (!hasMatchingTest) {
-      setOpenGate(true);
-    }
-  }, [hasMatchingTest]);
 
   //const actionsDisabled = useMemo(() => !hasMatchingTest, [hasMatchingTest]); 매칭검사 안했을 시
 
