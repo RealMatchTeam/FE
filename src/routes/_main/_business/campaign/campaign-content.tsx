4057de45d4209eb578702dc2209c364764ac139e
seyun31 <2ne1jenna@naver.com>
2026-01-29 23:09:12 +0900

    fix: arrow-right.svg import path in campaign-content

--- src/routes/_main/_business/campaign/campaign-content.tsx
+++ src/routes/_main/_business/campaign/campaign-content.tsx
@@ -7,7 +7,7 @@ import CampaignInfoGroup from "../components/CampaignInfoGroup";
 import editIcon from "../../../../assets/icon-edit.svg";
 import dropdownIcon from "../../../../assets/arrow-down.svg";
 import dropupIcon from "../../../../assets/arrow-up.svg";
-import arrowRightIcon from "../../../../assets/arrow-right.svg";
+import arrowRightIcon from "../../../../assets/icon/arrow-right.svg";
 import calendarIcon from "../../../../assets/icon-calender.svg";
 
 export default function CampaignContent() {
