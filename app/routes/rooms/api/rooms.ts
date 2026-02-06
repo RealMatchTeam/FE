cab3eea4effb809501616bf327d1429b35c715c9
yoonyoungyang <153082394+yoonyoungyang@users.noreply.github.com>
2026-02-06 14:31:49 +0900

    Update rooms.ts

--- app/routes/rooms/api/rooms.ts
+++ app/routes/rooms/api/rooms.ts
@@ -1,6 +1,6 @@
 import { axiosInstance } from "../../../api/axios";
 
-
+//채팅룸 생성 및 조회
 type CreateOrGetDirectRoomResponse = {
   isSuccess: boolean;
   code: string;
@@ -188,33 +188,4 @@ export async function getChatMessages({
     }
   );
   return res.data;
-}
-
-// 채팅 생성
-type CreateOrGetDirectRoomResponse = {
-  isSuccess: boolean;
-  code: string;
-  message: string;
-  result: {
-    roomId: number;
-    roomKey: string;
-    createdAt: string;
-  };
-};
-
-export async function createOrGetDirectRoom(body: {
-  brandId: number;
-  creatorId: number;
-}): Promise<CreateOrGetDirectRoomResponse["result"]> {
-  const res = await axiosInstance.post<CreateOrGetDirectRoomResponse>(
-    "/api/v1/chat/rooms",
-    body
-  );
-
-  const data = res.data;
-  if (!data?.isSuccess) {
-    throw new Error(data?.message ?? "채팅방 생성/조회 실패");
-  }
-
-  return data.result;
 }
