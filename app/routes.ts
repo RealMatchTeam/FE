638a714f3f567641d4022fff5443471b40620f40
Yoonchulchung <yoonchul005@gmail.com>
2026-02-08 22:19:38 +0900

    fix(#77): 채팅 라우팅 문제 해결

--- app/routes.ts
+++ app/routes.ts
@@ -55,7 +55,7 @@ export default [
 
     route("rooms", "routes/rooms/route.tsx", [
       route("brand/:brandId", "routes/rooms/$brandId.tsx"),
-      route("room/:roomId", "routes/rooms/$roomId.tsx"),
+      route(":roomId", "routes/rooms/$roomId.tsx"),
     ]),
 
     route("mypage", "routes/mypage/route.tsx", [
